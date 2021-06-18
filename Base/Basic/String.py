331c5651c07ace16e9f943c36b5239374be80b77
Lee Jun Gyu <leejg@renosoft.kr>
2021-06-18 13:03:10 +0900

    문자열 인덱스 변형

--- Base/Basic/String.py
+++ Base/Basic/String.py
@@ -51,3 +51,34 @@ print("i ate %s apples. So, I was sick for %s days" % (3, "three"))
 
 # 포매팅 연산자가 있다면 뒤에 %를 표현하려면 %% 두번 사용해야함
 print("%d%%" % 100)
+
+# 정렬과 공백
+# 10칸 띄어쓰기
+print("%10s" % 'hi')
+# 10칸 반대로 띄어쓰기
+print("%-10sjane" % 'hi')
+
+print("I eat {0} apples".format(3))
+
+# 인덱스와 name 혼합
+print("I ate {0} apples. So, I was sick for {day} days".format(10, day=3))
+# {} 해당 번호는 인덱스 번호
+print("I ate {0} apples. So, I was sick for {1} days".format(10, 3))
+
+# :< :> 좌우정렬 :^ 가운데 정렬
+print("{0:^10}".format("hi"))
+
+# f'문자열' formatting 을 선언하고 표현할 수 있다
+name = 'jun'
+age = 28
+print(f'나의 이름은 {name}이고, {age}살 입니다')
+
+# 문자 개수 세기
+text = "hobby"
+print(text.count('b'))
+# 인덱스 위치 찾기
+print(text.find('h'))
+# index는 문자열을 찾지 못하면 오류 발생
+print(text.index('h'))
+
+print(",".join('abcd'))
