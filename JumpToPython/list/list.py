e64d143c7943116ef783714184d662189c4d8669
SpellBound <junlee36520620@gmail.com>
2021-07-07 23:29:44 +0900

    리스트 끝

--- JumpToPython/list/list.py
+++ JumpToPython/list/list.py
@@ -9,8 +9,28 @@ print(b[2][2][0])
 
 # list 연산
 c = [1, 2, 3]
-d = [4, 5, 6]
+d = [4, 6, 5]
 print(c + d)
-
 print(c * 3)
+print(len(c))
+
+print(str(c[2]) + "hi")
+
+# 리스트 요소 삭제 del 객체, remove, pop
+del c[1]
+print(c)
+
+d.sort()
+d.reverse()
+
+print(d)
+
+# 위치 반환
+print(a.index(1))
+
+a = [1, 2, 3, 4]
+b = [5, 6, 7, 8]
+a.extend(b)
+print(a)
+
 
