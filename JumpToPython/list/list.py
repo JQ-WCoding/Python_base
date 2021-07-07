687cf6c7181f81f1df6c2697d0bbc855013a5890
SpellBound <junlee36520620@gmail.com>
2021-07-07 23:23:05 +0900

    리스트 관련

---
+++ JumpToPython/list/list.py
@@ -0,0 +1,16 @@
+# list indexing
+a = [1, 2, 3, ['a', 'b', 'c']]
+print(a[3])
+print(a[-1])
+
+# triple inner
+b = [1, 2, ['a', 'b', ['Life', 'is']]]
+print(b[2][2][0])
+
+# list 연산
+c = [1, 2, 3]
+d = [4, 5, 6]
+print(c + d)
+
+print(c * 3)
+
