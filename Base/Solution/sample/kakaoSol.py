da68338279cedcc613d29fc20e52462b78612e92
SpellBound <junlee36520620@gmail.com>
2021-07-13 22:51:01 +0900

    kakao 코테 문제 python으로 해결해보기

---
+++ Base/Solution/sample/kakaoSol.py
@@ -0,0 +1,25 @@
+import re
+
+# "abcabcabc" "abc"
+
+
+str2 = "abcabcabc"
+
+
+def mySol(line1, line2):
+    conunt = 0
+    for i in range(len(line1)):
+        p2 = '.{%d}' % i
+        p3 = p2.join(line2)
+        print(p3)
+        answer1 = re.findall(r'(?=%s)' % p3, line1)
+        for j in answer1:
+            conunt += 1
+    return conunt
+
+
+print(mySol("abbbcbbb", "bbb"))
+print(mySol("abcabcabc", "abc"))
+print(mySol("abacaba", "acb"))
+# 내가 추가로 궁금해서 만들어 본것
+print(mySol("cccc", "ccc"))
